873a103b235530092e3f24a1e2a3c33a8a20cdcc
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2025-05-28 09:49:07 +0700

    feat: setup shadcn ui and some hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+import useFollowPointer from './use-follow-pointer'
+import useHasMounted from './use-has-mounted'
+import useScreenSize from './use-screen-size'
+
+export { useFollowPointer, useHasMounted, useScreenSize }
---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+import { cn } from './styles-helper'
+
+export { cn }
